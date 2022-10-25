04c64561d9d17dac7ec725d9b795c669b2447e47
Jayesh Kawli <jkawli@tripactions.com>
2022-10-25 08:43:53 +0200

    Initial commit

---
+++ SwiftUIMVVMDemo/SwiftUIMVVMDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIMVVMDemoApp.swift
+//  SwiftUIMVVMDemo
+//
+//  Created by Jayesh Kawli on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //sss
+        }
+    }
+}
